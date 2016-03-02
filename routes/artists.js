444fe15e6caccaa4eb3a9805169aa035033cdfee
Emmerlord <amras15@gmail.com>
2016-03-02 21:43:04 +0000

    Fixed artworks not displaying some artworks

--- routes/artists.js
+++ routes/artists.js
@@ -8,24 +8,30 @@ var Thread = require('../models/artworks.js');
 
 /* GET artists listing. */
 router.get('/', function(req, res, next) {
-  mongoose.model('artists').find(function(err,artists){
+  mongoose.model('artists').find(function(err, artists) {
 
-      res.render('artists', {  json: artists });
+    res.render('artists', {
+      json: artists
+    });
 
- });
- });
+  });
+});
 
 
 /* GET artists listing. */
 router.get('/:id', function(req, res, next) {
-  mongoose.model('artists').find({id: req.params.id},function(err,artists){
+  mongoose.model('artists').find({
+    id: req.params.id
+  }, function(err, artists) {
 
 
-      res.render('artists', {  json: artists });
+    res.render('artists', {
+      json: artists
+    });
 
 
- });
- });
+  });
+});
 
 
 
